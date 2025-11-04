bfe7b706439109ab648c94a0776482a186cad6b2
andrei perepiolkin <andrei.perepiolkin@open-e.com>
2025-11-04 06:27:15 -0500

    Update version

--- jdssc/setup.py
+++ jdssc/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='OpenEJovianDSSProxmoxCLI',
-    version='0.10.7',
+    version='0.10.8',
     author='Andrei Perepiolkin',
     author_email='andrei.perepiolkin@open-e.com',
     packages=['jdssc'],
