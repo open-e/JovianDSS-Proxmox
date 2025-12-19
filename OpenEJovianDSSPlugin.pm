05449cfa5bdf4ffad90ab1f5d310481a05a8dcd2
andrei perepiolkin <andrei.perepiolkin@open-e.com>
2025-12-18 23:40:50 -0500

    v0.10.13

--- OpenEJovianDSSPlugin.pm
+++ OpenEJovianDSSPlugin.pm
@@ -46,7 +46,7 @@ use base                   qw(PVE::Storage::Plugin);
 
 use constant COMPRESSOR_RE => 'gz|lzo|zst';
 
-my $PLUGIN_VERSION = '0.10.12';
+my $PLUGIN_VERSION = '0.10.13';
 
 #    Open-E JovianDSS Proxmox plugin
 #
