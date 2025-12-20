7728fea552c726760585b9618f3a2f6ae17ca201
Andrei <andrei.perepiolkin@open-e.com>
2025-12-20 18:17:00 -0500

    Expand install script with allow-downgrade assume-yes flags (#54)
    * Add assume-yes flag
    
    * Add allow-downgrades flag to install script
    
    * Update docs

--- install.pl
+++ install.pl
@@ -47,7 +47,9 @@ my $HELP = 0;
 my $ADD_DEFAULT_MULTIPATH_CONFIG = 0;
 my $FORCE_MULTIPATH_CONFIG = 0;
 my $USE_REINSTALL = 0;  # Default to NOT using --reinstall flag
+my $ALLOW_DOWNGRADES = 0;  # Default to NOT allowing downgrades
 my $VERBOSE = 0;  # Default to non-verbose output
+my $ASSUME_YES = 0;  # Default to interactive confirmation
 my @WARNING_NODES = ();  # Track nodes with warnings
 my @SKIPPED_NODES = ();  # Track nodes where operations were skipped
 
@@ -69,6 +71,8 @@ GetOptions(
     "add-default-multipath-config" => \$ADD_DEFAULT_MULTIPATH_CONFIG,
     "force-multipath-config" => \$FORCE_MULTIPATH_CONFIG,
     "reinstall"     => \$USE_REINSTALL,
+    "allow-downgrades" => \$ALLOW_DOWNGRADES,
+    "assume-yes"    => \$ASSUME_YES,
     "verbose|v"     => \$VERBOSE,
     "help|h"        => \$HELP,
 ) or die "Error parsing options\n";
@@ -128,6 +132,8 @@ General:
   --add-default-multipath-config  Install default multipath configuration
   --force-multipath-config   Overwrite existing multipath config files
   --reinstall                Use --reinstall apt flag (default: disabled)
+  --allow-downgrades         Allow installing older package versions
+  --assume-yes               Automatic yes to prompts (non-interactive mode)
   -v, --verbose              Show detailed output during installation/removal
   -h, --help                 Show this help
 
@@ -719,6 +725,7 @@ sub get_apt_install_command {
     my $package_path = shift;
     my $cmd = "apt-get -y -q";
     $cmd .= " --reinstall" if $USE_REINSTALL;
+    $cmd .= " --allow-downgrades" if $ALLOW_DOWNGRADES;
     $cmd .= " install $package_path";
     return $cmd;
 }
@@ -1244,10 +1251,12 @@ sub main {
             my @sorted_nodes = sort { $a->{name} cmp $b->{name} } @nodes;
             print_operation_nodes("Plugin will be removed from the following nodes:", \@sorted_nodes);
 
-            my $confirm = simple_readline("Continue? (y/n): ");
-            unless ($confirm && $confirm =~ /^y$/i) {
-                say "Operation cancelled.";
-                return 0;
+            unless ($ASSUME_YES) {
+                my $confirm = simple_readline("Continue? (y/n): ");
+                unless ($confirm && $confirm =~ /^y$/i) {
+                    say "Operation cancelled.";
+                    return 0;
+                }
             }
             say "";
 
@@ -1300,10 +1309,12 @@ sub main {
             my @sorted_nodes = sort { $a->{name} cmp $b->{name} } @nodes;
             print_operation_nodes("Plugin $TAG will be installed on the following nodes:", \@sorted_nodes);
 
-            my $confirm = simple_readline("Continue? (y/n): ");
-            unless ($confirm && $confirm =~ /^y$/i) {
-                say "Operation cancelled.";
-                return 0;
+            unless ($ASSUME_YES) {
+                my $confirm = simple_readline("Continue? (y/n): ");
+                unless ($confirm && $confirm =~ /^y$/i) {
+                    say "Operation cancelled.";
+                    return 0;
+                }
             }
             say "";
 
