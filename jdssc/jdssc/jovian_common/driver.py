2da5bd17d029b823f5f1b3a0d3d1910b392dce09
Andrei Perepiolkin <andrei.perepiolkin@open-e.com>
2026-05-27 20:21:01 +0200

    Update version to v0.11.5

--- jdssc/jdssc/jovian_common/driver.py
+++ jdssc/jdssc/jovian_common/driver.py
@@ -40,7 +40,7 @@ class JovianDSSDriver(object):
 
     def __init__(self, config):
 
-        self.VERSION = "0.11.4"
+        self.VERSION = "0.11.5"
 
         self.configuration = config
         self._pool = self.configuration.get('jovian_pool', 'Pool-0')
--- jdssc/setup.py
+++ jdssc/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='OpenEJovianDSSProxmoxCLI',
-    version='0.11.4',
+    version='0.11.5',
     author='Andrei Perepiolkin',
     author_email='andrei.perepiolkin@open-e.com',
     packages=['jdssc'],
